ad98202d87d71a3c523ce2e0e22cbf7885cb8313
Hiru512 <hyeonmini512@gmail.com>
2026-04-07 02:31:11 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,95 @@
+'''봇 로직'''
+
+from ramen_data import ramen_data
+
+
+'''라이브러리 import
+        *의미*'''
+import os
+import discord #discord -> 디스코드 API 연결용
+from discord.ext import commands #commands -> 명령어 시스템(!hello) 쉽게 만들기
+
+
+'''intents (핵심개념)
+        *구조*
+디스코드 ->  "어떤 정보까지 볼 수 있게 할지" 제한됨'''
+intents = discord.Intents.default() #default() -> 기본 권한 
+intents.message_content = True # message_content = True -> 메시지 내용 읽기 허용
+
+
+'''봇 생성
+        *의미*'''
+bot = commands.Bot(command_prefix="!", intents=intents)
+#command_prefix="!" -> !hello 이런 식으로 명령어 시작
+#intents -> 아까 설정한 권한 적용
+
+
+'''on_ready(이벤트)
+        *구조*
+        봇 실행 완료 -> 이벤트 발생 -> 함수 실행
+        *역할*
+        - 봇 로그인 확인
+        - 디버깅용(정상 작동 체크)'''
+@bot.event
+async def on_ready():
+    print(f"로그인 완료: {bot.user}")
+
+
+    '''on_message'''
+@bot.event
+async def on_message(message):
+    if message.author.bot:      #자기 자신 무시 -> 봇끼리 무한루프 방지
+        return
+    
+    await bot.process_commands(message)
+
+
+def create_ramen_command(ramen_type):
+    async def command(ctx):
+        shops = ramen_data[ramen_type]
+        names = [shop["name"] for shop in shops]
+
+        if names:
+            msg = "\n".join(names)
+            await ctx.send(f"🍜 {ramen_type} 라멘 리스트: \n{msg}")
+        else:
+            await ctx.send(f"🍜 {ramen_type} 라멘집 정보가 없습니다.")
+
+        return command
+    
+    for ramen_type in ramen_data.keys():
+        bot.command(name=f"{ramen_type}라멘")(create_ramen_command(ramen_type))
+
+
+'''명령어 만들기
+        *구조*'''
+@bot.command()      # @bot.comand() -> "이건 명령이다" 표시
+async def 라멘(ctx, *, name):
+    for shops in ramen_data.values():
+        for shop in shops:
+            if shop["name"] == name:
+                msg = (
+                    f"🍜{shop['name']}\n"
+                    f"영업시간: {shop['Hours']}\n"
+                    f"브레이크타임: {shop['Breaktime']}\n"
+                    f"라스트오더: {shop['L.O']}\n"
+                    f"캐치테이블: {shop['Catchtable']}\n"
+                    f"인스타그램: {shop['Instagram']}\n"
+                )      # ctx (context) -> 누가, 어디서, 어떤 메시지 보냈는지 정보
+                await ctx.send(msg)     # await ctx.send() -> 디스코드 채팅으로 메시지 보내기
+                return
+        await ctx.send("해당 라멘집이 없습니다. 추후에 업데이트 될 예정입니다.")
+
+
+@bot.command()
+async def woongseong(ctx):
+    await ctx.send("웅성!")
+
+    
+    ###print(message.content)      #터미널에서 메시지 출력
+    ###await bot.process_commands(message)     #이거 없으면 명령어 시스템 꺼짐
+
+#역할 1: 모든 메시지 들어올 때마다 실행됨
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
---
+++ ramen_data.py
@@ -0,0 +1,233 @@
+'''라멘 데이터'''
+
+
+ramen_data = {
+    "쇼유": [
+        {
+            "name": "라멘바 시코우",
+            "Hours": "12:00 ~ 18:40(60그릇 소진시 마감)",
+            "breaktime": "14:30 ~ 17:00",
+            "L.O": "14:20, 18:30",
+            "Catchtable": "점심 원격 대기: 11:30, 저녁 원격 대기: 16:30",
+            "Instagram": "https://www.instagram.com/ramenbarshikou/"
+        },
+        {
+            "name": "요아케",
+            "Hours": "11:30 ~ 21:00(매주 월요일, 화요일 휴무)",
+            "Breaktime": "15:00 ~ 17:30",
+            "L.O": "20:30",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/yoake.kr/"
+        },
+        {
+            "name": "소바하우스 멘야준",
+            "Hours": "11:00 ~ 20:00",
+            "Breaktime": "15:00 ~ 17:00(주말 없음)",
+            "L.O": "없음",
+            "Catchtable": "원격 대기: 11:00~",
+            "Instagram": "https://www.instagram.com/sobahousemenyajoon/"
+        },
+        {
+            "name": "멘큐단",
+            "Hours": "11:30 ~ 20:00(매주 월요일, 화요일 휴무)",
+            "Breaktime": "정보없음",
+            "L.O": "19:30",
+            "Catchtable": "점심 현장 대기: 09:30~, 점심 온라인 대기: 11:30~",
+            "Instagram": "https://www.instagram.com/menqdan"
+        }
+    ],
+    "시오": [
+        {
+            "name": "라멘바 시코우",
+            "Hours": "12:00 ~ 18:40(60그릇 소진시 마감)",
+            "breaktime": "14:30 ~ 17:00",
+            "L.O": "14:20, 18:30",
+            "Catchtable": "점심 원격 대기: 11:30, 저녁 원격 대기: 16:30",
+            "Instagram": "https://www.instagram.com/ramenbarshikou/"
+        },
+        {
+            "name": "요아케",
+            "Hours": "11:30 ~ 21:00(매주 월요일, 화요일 휴무)",
+            "Breaktime": "15:00 ~ 17:30",
+            "L.O": "20:30",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/yoake.kr/"
+        },
+        {
+            "name": "희옥",
+            "Hours": "11:00 ~ 20:00(월~금), 11:00 ~ 15:00(토요일),(매주 일요일 휴무)",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "14:30, 19:30(토요일: 14:30)",
+            "Catchtable": "점심: 09:00~(현장 대기), 09:30~(원격 대기), 저녁: 16:00~(현장 대기), 16:30~(원격 대기)",
+            "Instagram": "https://www.instagram.com/heeok_official/"
+        },
+        {
+            "name": "롱시즌",
+            "Hours": "11:30 ~ 20:00(매주 월요일, 화요일 휴무)",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "14:30, 19:30",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/ramenlongseason/"
+        },
+        {
+            "name": "킨카",
+            "Hours": "11:00~20:00(평일), 11:30~16:00(토요일), (매주 일요일 휴무)",
+            "Breaktime": "15:00 ~ 17:30",
+            "L.O": "14:30, 15:30",
+            "Catchtable": "점심: 10:00~(현장 대기), 11:10~(원격 대기, 토요일은 11:40~ 원격 대기), 저녁: 17:00~(현장 대기), 17:40~(원격 대기)",
+            "Instagram": "https://www.instagram.com/kinka.ramen_official/"
+        },
+        {
+            "name": "멘야준",
+            "Hours": "11:00 ~ 20:00",
+            "Breaktime": "없음",
+            "L.O": "20:00",
+            "Catchtable": "원격 대기: 11:00~",
+            "Instagram": "https://www.instagram.com/menyajoon/"
+        },
+        {
+            "name": "반라이",
+            "Hours": "11:00 ~ 20:00(매주 화요일 휴무)",
+            "Breaktime": "없음",
+            "L.O": "20:00",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/ramen_banrai/"
+        }
+    ],
+    "돈코츠": [
+        {
+            "name": "맛스구",
+            "Hours": "11:00 ~ 21:00(매주 월요일 휴무)",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "20:30",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/matsugu24/"
+        },
+        {
+            "name": "쿄",
+            "Hours": "11:30 ~ 19:30(매주 월요일 휴무)",
+            "Breaktime": "14:50 ~ 17:00",
+            "L.O": "재료 소진 시 조기마감",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/kyoramen/"
+        },
+        {
+            "name": "반라이",
+            "Hours": "11:00 ~ 20:00(매주 화요일 휴무)",
+            "Breaktime": "없음",
+            "L.O": "20:00",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/ramen_banrai/"
+        },
+        {
+            "name": "보루도",
+            "Hours": "11:30 ~ 20:30(월~목), 11:30~20:30(금~일)",
+            "Breaktime": "16:00~17:30(월~목)",
+            "L.O": "20:00",
+            "Catchtable": "11:30~",
+            "Instagram": "https://www.instagram.com/ramen_borudo/"
+        }
+    ],
+    "지로": [
+        {
+            "name": "류진",
+            "Hours": "12:00 ~ 21:30(평일), 11:00 ~ 21:30(주말)",
+            "Breaktime": "14:00 ~ 17:30",
+            "L.O": "14:00, 21:30",
+            "Catchtable": "11:00~(평일), 10:00~(주말)",
+            "Instagram": "https://www.instagram.com/ryujin_ramen_seoul/"
+        },
+        {
+            "name": "566",
+            "Hours": "11:00 ~ 20:30",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "14:50, 20:00",
+            "Catchtable": "11:00~, 16:00~",
+            "Instagram": "https://www.instagram.com/566ramen/"
+        },
+        {
+            "name": "오레노이키루미치",
+            "Hours": "11:00 ~ 22:00",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "21:40",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/orenoikirumichi_kr/"
+        }
+    ],
+    "미소": [
+        {
+            "name": "아키야",
+            "Hours": "11:30 - 21:00(매주 일요일 휴무)",
+            "Breaktime": "16:00-17:00(토요일, 공휴일 없음)",
+            "L.O": "20:30",
+            "Catchtable": "11:30~",
+            "Instagram": "https://www.instagram.com/akiya.ramen/"
+        },
+        {
+            "name": "부타노맥스",
+            "Hours": "11:00 ~ 00:30(매주 토요일, 일요일 휴무)",
+            "Breaktime": "14:00 ~ 17:30",
+            "L.O": "00:00",
+            "Catchtable": "현장 웨이팅",
+            "Instagram": "https://www.instagram.com/butanomax/"
+        },
+        {
+            "name": "멘타미",
+            "Hours": "11:00 ~ 20:00",
+            "Breaktime": "15:00 ~ 17:00",
+            "L.O": "14:30, 20:00",
+            "Catchtable": "현장 웨이팅",
+            "Instagram": "https://www.instagram.com/mentami_official/"
+        }
+    ],
+    "츠케멘": [
+        {
+            "name": "힛사츠와자",
+            "Hours": "11:30 ~ 20:00(매주 월요일 휴무)",
+            "Breaktime": "14:30 ~ 17:00",
+            "L.O": "14:30, 19:30",
+            "Catchtable": "11:30~, 17:00~",
+            "Instagram": "https://www.instagram.com/hissatsuwaza_/"
+        },
+        {
+            "name": "쿄",
+            "Hours": "11:30 ~ 19:30(매주 월요일 휴무)",
+            "Breaktime": "14:50 ~ 17:00",
+            "L.O": "재료 소진 시 조기마감",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/kyoramen/"
+        },
+        {
+            "name": "진세이",
+            "Hours": "11:30 ~ 20:30(화~토), 11:30 ~ 17:00(일), (매주 월요일 휴무)",
+            "Breaktime": "15:00 ~ 17:00(일요일은 없음)",
+            "L.O": "14:30, 20:00",
+            "Catchtable": "11:30~, 17:00~",
+            "Instagram": "https://www.instagram.com/jinseiramen_yeonnam/"
+        },
+        {
+            "name": "반라이",
+            "Hours": "11:00 ~ 20:00(매주 화요일 휴무)",
+            "Breaktime": "없음",
+            "L.O": "20:00",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/ramen_banrai/"
+        },
+        ###{
+            ###"Name": "멘타카무쇼",
+            ###"Hours": "",
+            ###"Breaktime": "",
+            ###"L.O": "",
+            ###"Catchtable": "",
+            ###"Instagram": ""
+        ###},
+        {
+            "name": "멘도무카우",
+            "Hours": "11:30 ~ 17:00(매주 수요일 휴무)",
+            "Breaktime": "없음",
+            "L.O": "16:30",
+            "Catchtable": "없음",
+            "Instagram": "https://www.instagram.com/mendo_mukau/"
+        }
+    ]
+}
